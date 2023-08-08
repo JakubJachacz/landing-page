eed9b600d6bbb47807ba9f1bed6deeb231fce7a2
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-08 16:48:51 +0200

    add Container component

---
+++ src/Container/index.js
@@ -0,0 +1,15 @@
+import TopBar from "../TopBar"; 
+import Section from "./Section";
+import Slideshow from "./Slideshow";
+
+const Container = () => {
+  return (
+    <> 
+    <TopBar />
+      <Slideshow />
+      <Section></Section>
+    </>
+  );
+};
+
+export default Container;
