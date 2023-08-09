000c61a7140c0583e644a58226fa1556cde74d50
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-10 01:02:55 +0200

    add Footer styles

---
+++ src/Container/Footer/styled.js
@@ -0,0 +1,37 @@
+import { styled } from "styled-components";
+
+export const FooterContainer = styled.div`
+max-width: 1440px;
+height: 509px;
+background-color: #D9D9D9;
+margin: 0 auto;
+`
+
+export const FooterWrapper = styled.div`
+display: grid;
+grid-template-columns: 181px 446px 710px;
+`;
+
+export const Contact = styled.div`
+color: #181818;
+font-size: 18px;
+line-height: 24px;
+background-color: cyan;
+padding: 84px 60px 315px;
+`;
+
+export const QuestionsContainer = styled.div`
+color: #474945;
+font-size: 18px;
+line-height: 24px;
+`;
+
+export const QuestionsTitle = styled.p`
+color: #181818;
+font-size: 36px;
+line-height: 39px;
+`;
+
+export const QuestionsText = styled.p`
+
+`;
