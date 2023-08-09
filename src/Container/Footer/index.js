c434a28ad46e4ab1de97a547ce3d431d6e4ca095
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-10 01:02:49 +0200

    add Footer component

---
+++ src/Container/Footer/index.js
@@ -0,0 +1,16 @@
+import { Contact, FooterContainer, FooterWrapper } from "./styled";
+
+const Footer = () => {
+
+    return (
+ <FooterContainer>
+<FooterWrapper>
+    <Contact>Kontakt</Contact>
+</FooterWrapper>
+
+ </FooterContainer>
+    );
+  };
+  
+  export default Footer;
+  
