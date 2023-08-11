fa5d9482d69943618ea6669386d55611f12e7d83
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-11 05:19:01 +0200

    add more components to Footer

--- src/Container/Footer/index.js
+++ src/Container/Footer/index.js
@@ -1,16 +1,41 @@
-import { Contact, FooterContainer, FooterWrapper } from "./styled";
+import {
+  Contact,
+  FooterContainer,
+  FooterWrapper,
+  Maps,
+  QuestionsContainer,
+  Info,
+  QuestionsTitle,
+  FooterText,
+} from "./styled";
 
 const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Contact>Kontakt</Contact>
+        <QuestionsContainer>
+          <QuestionsTitle>Masz pytania?</QuestionsTitle>
+          <Info>Safeko house sp. z o.o.</Info>
+          <Info>ul. Mackiewicza 21/31, 20-865 Lublin</Info>
+          <Info isMiddle>kontakt@safekohouse.com</Info>
+          <Info>+48 609 505 111</Info>
+          <Info isLast>NIP: 712 338 48 46</Info>
+          <Info>KRS: 0000779078</Info>
+        </QuestionsContainer>
+        <Maps />
+        <Footer>
+          <FooterText>© Safeko house</FooterText>
+          <FooterText>Polityka prywatności</FooterText>
+          <FooterText>
+            Stosujemy pliki cookies. Korzystanie ze strony bez zmiany ustawień
+            przeglądarki oznacza, że pliki cookies będą zamieszczane w Twoim
+            urządzeniu.
+          </FooterText>
+        </Footer>
+      </FooterWrapper>
+    </FooterContainer>
+  );
+};
 
-    return (
- <FooterContainer>
-<FooterWrapper>
-    <Contact>Kontakt</Contact>
-</FooterWrapper>
-
- </FooterContainer>
-    );
-  };
-  
-  export default Footer;
-  
+export default Footer;
