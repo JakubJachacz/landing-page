f5d7bed01f692aeeca79db97696e84c1baf39e41
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-12 03:27:31 +0200

    add media query

--- src/TopBar/styled.js
+++ src/TopBar/styled.js
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 import { ReactComponent as Aura_Favicon } from "../images/Aura_Favicon.svg"
 import { ReactComponent as Logo } from "../images/logo_safeko.svg"
+import NavIcon from "../images/navigation.svg"
 
 export const StyledTopBar = styled.div`
 display: flex;
 justify-content: space-between;
 width: 100%;
 height: 80px;
+
+@media (max-width: 750px) {
+    height: 70px;
+  }
 `;
 
 export const Title = styled.div`
 display: flex;
 align-items: center;
 padding: 15px 60px; 
-`;  
 
-export const StyledIcon = styled(Aura_Favicon)`
-width: 50px;
-height: 50px;
-`;
+@media (max-width: 750px) {
+    padding: 15px 30px;
+  }
+`;   
 
 export const StyledNavigation = styled.nav`
 display: flex;
@@ -27,6 +31,10 @@ justify-content: flex-end;
 max-width: 410px;
 width: 100%;
 margin-right: 40px;
+
+@media (max-width: 750px) {
+    margin: 0;
+  }
 `;
 
 export const NavItem = styled.button`
@@ -35,10 +43,31 @@ color: #71746E;
 font-size: 17px;
 background-color: inherit;
 border: none;
+
+@media (max-width: 750px) {
+    display: none;
+  }
 `;
 
 
 export const StyledLogo = styled(Logo)`
 width: 210px;
 height: 50px;
+
+@media (max-width: 750px) {
+     width: 160px;
+     height: 25px;
+  }
+`;
+
+export const NavButton = styled.div`
+width: 70px;
+height: 70px; 
+background-image: url(${NavIcon});
+background-repeat: no-repeat;
+background-position: center;
+
+@media (min-width: 751px) {
+    display: none;
+  }
 `;
