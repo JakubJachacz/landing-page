b6996307cf9c344925ac87a28019d9aa36aacbc3
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-13 03:29:01 +0200

    add breakpoints and ButtonsContainer

--- src/Container/Slideshow/styled.js
+++ src/Container/Slideshow/styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import MainPicture from "../../images/Photo_01.png";
+import MainPictureSmall from "../../images/Photo_01_small.png";
 import { ReactComponent as ArrowLeft } from "../../images/VectorLeft.svg";
 import { ReactComponent as ArrowRight } from "../../images/VectorRight.svg";
 
@@ -9,15 +10,21 @@ export const StyledSlideshow = styled.div`
   max-width: 1400px;
   width: 100%;
   max-height: 700px;
-  height: 100%; 
+  height: 100%;
   margin: 0 auto;
 
   @media (max-width: 1440px) {
     margin: 0 20px;
   }
+
+  @media (max-width: 750px) {
+    width: 340px;
+    height: 500px;
+    margin: 0 10px;
+  }
 `;
 
-export const MainPictureContainer = styled.div`
+export const MainPictureContainer = styled.div` 
   max-width: 1400px;
   width: 100%;
   max-height: 700px;
@@ -27,24 +34,38 @@ export const MainPictureContainer = styled.div`
   background-image: url(${MainPicture});
   position: relative;
   aspect-ratio: 2 / 1;
+
+  @media (max-width: 750px) {
+    margin: 0 10px;
+    background-image: url(${MainPictureSmall});
+  }
 `;
 
 export const Pagination = styled.span`
   max-width: 355px;
   width: 100%;
   height: 80px;
-  background-color: #181818; 
+  background-color: #181818;
   position: absolute;
   bottom: 0;
   right: 0;
   display: flex;
   gap: 30px;
+
+  @media (max-width: 750px) {
+    width: 340px;
+    height: 60px;
+    gap: 0;
+    justify-content: space-between;
+  }
 `;
 
-export const ButtonsContainer = styled.div`
-  max-width: 203px;
+export const ButtonsContainer = styled.span`
+  max-width: 192px;
   width: 100%;
-  height: 80px;
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
 `;
 
 export const PaginationButton = styled.button`
@@ -54,6 +75,11 @@ export const PaginationButton = styled.button`
   display: flex;
   background-color: inherit;
   border: none;
+
+  @media (max-width: 750px) {
+    height: 60px;
+    width: 60px;
+  }
 `;
 
 export const PaginationCounter = styled.p`
@@ -63,6 +89,11 @@ export const PaginationCounter = styled.p`
   display: flex;
   align-items: center;
   padding: 10px 30px 10px 45px;
+
+  @media (max-width: 750px) {
+    font-size: 16px;
+    line-height: 23px;
+  }
 `;
 
 export const StyledArrowLeft = styled(ArrowLeft)`
