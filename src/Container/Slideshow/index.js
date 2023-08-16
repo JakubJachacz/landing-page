5e6fe2d793f0cf1e9f99c237ad1812ef484b729a
Jakub Jachacz <hexadoxpl@gmail.com>
2023-08-16 06:23:44 +0200

    add functional pagination with image swap

--- src/Container/Slideshow/index.js
+++ src/Container/Slideshow/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   ButtonsContainer,
   MainPictureContainer,
@@ -6,23 +7,49 @@ import {
   PaginationCounter,
   StyledArrowLeft,
   StyledArrowRight,
-  StyledSlideshow,
+  StyledSlideshow, 
 } from "./styled";
+import image1 from "../../images/Photo_01.png";
+import image2 from "../../images/Photo_01_two.png";
+
+const images = [
+  image1,
+  image2,
+];
 
 const Slideshow = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const isAtFirstImage = currentIndex === 0;
+  const isAtLastImage = currentIndex === images.length - 1;
+
   return (
     <StyledSlideshow>
-      <MainPictureContainer>
+      <MainPictureContainer imageUrl={images[currentIndex]}>
         <Pagination>
           <ButtonsContainer>
-            <PaginationButton>
-              <StyledArrowLeft />
+            <PaginationButton onClick={handlePrevSlide} disabled={isAtFirstImage}>
+              <StyledArrowLeft disabled={isAtFirstImage}/>
             </PaginationButton>
-            <PaginationButton>
-              <StyledArrowRight />
+            <PaginationButton onClick={handleNextSlide} disabled={isAtLastImage}>
+              <StyledArrowRight disabled={isAtLastImage}/>
             </PaginationButton>
           </ButtonsContainer>
-          <PaginationCounter>01/02</PaginationCounter>
+          <PaginationCounter>
+            {currentIndex + 1}/{images.length}
+          </PaginationCounter>
         </Pagination>
       </MainPictureContainer>
     </StyledSlideshow>
